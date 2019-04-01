71156095746621a28638f7600414eca1aa66b332
simreaper <simreaper0@gmail.com>
2019-04-01 21:59:40 +0530

    Update MyProgram.java

--- MyProgram.java
+++ MyProgram.java
@@ -6,10 +6,10 @@ String[] WordList3 = {"Work","Task","complete promise","follow the path"};
 int length1 = WordList1.length;
 int length2 = WordList2.length;
 int length3 = WordList3.length;
-int rand1 = (int) (Math.random() * length1);
-int rand2 = (int) (Math.random() * length2);
-int rand3 = (int) (Math.random() * length3);
-String phrase = WordList1[rand1] + " " + WordList2[rand2] + " " + WordList3[rand3];
+int random1 = (int) (Math.random() * length1);
+int random2 = (int) (Math.random() * length2);
+int random3 = (int) (Math.random() * length3);
+String phrase = WordList1[random1] + " " + WordList2[random2] + " " + WordList3[random3];
 
 System.out.println("Hello " + phrase);
 
